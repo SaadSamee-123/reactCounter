7693c4d1a8b68a28324d62fd392a66cc9d5fbbff
SaadSamee-123 <saadsameequreshi@gmail.com>
2022-09-22 11:29:39 +0500

    first counter

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const counterIncremant = () => {
+    setCounter(counter + 1);
+  };
+
+  const counterDecrement = () => {
+    return counter <= 0 ? setCounter(0) : setCounter(counter - 1);
+  };
+  return (
+    <div className="text-center container forSize">
+      <div className="text-center">
+        <div>
+          <h1 className="mt-5">{counter}</h1>
+        </div>
+        <div>
+          <button className="btn btn-primary mt-3" onClick={counterIncremant}>
+            Increment
+          </button>
+          <button
+            className="btn btn-danger mt-3 mx-2"
+            onClick={counterDecrement}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
